bb98ec10270bff204e700d053a4cbcab31d32e53
YoDiGutY1 <101413720+YoDiGutY1@users.noreply.github.com>
2022-03-11 12:35:39 -0500

    Update routers.go

--- routers/routers.go
+++ routers/routers.go
@@ -7,14 +7,14 @@ import (
 	"net/http"
 	"path/filepath"
 
-	"github.com/penggy/EasyGoLib/db"
+	"github.com/YoDiGutY1/EasyGoLib/db"
 
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
-	"github.com/penggy/EasyGoLib/utils"
+	"github.com/YoDiGutY1/EasyGoLib/utils"
 	"github.com/penggy/cors"
-	"github.com/penggy/sessions"
+	"github.com/YoDiGutY1/sessions"
 	validator "gopkg.in/go-playground/validator.v8"
 )
 
