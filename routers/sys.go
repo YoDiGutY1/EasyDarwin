2cc91ee383e7f8dc9ab482691e455bb09ce3a583
YoDiGutY1 <101413720+YoDiGutY1@users.noreply.github.com>
2022-03-11 12:37:11 -0500

    Update sys.go

--- routers/sys.go
+++ routers/sys.go
@@ -8,12 +8,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/EasyDarwin/EasyDarwin/models"
-	"github.com/EasyDarwin/EasyDarwin/rtsp"
+	"github.com/YoDiGutY1/EasyDarwin/models"
+	"github.com/YoDiGutY1/EasyDarwin/rtsp"
 	"github.com/gin-gonic/gin"
-	"github.com/penggy/EasyGoLib/db"
-	"github.com/penggy/EasyGoLib/utils"
-	"github.com/penggy/sessions"
+	"github.com/YoDiGutY1/EasyGoLib/db"
+	"github.com/YoDiGutY1/EasyGoLib/utils"
+	"github.com/YoDiGutY1/sessions"
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/mem"
 )
